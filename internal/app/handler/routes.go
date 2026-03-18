ea78ac4fadb39b79bedb2341a4a724831030c173
pipapipapa <andusewp@gmail.com>
2026-03-18 14:20:03 +0300

    Очепятка

--- internal/app/handler/routes.go
+++ internal/app/handler/routes.go
@@ -15,8 +15,8 @@ func (h *Handler) RegisterAPI(router *gin.Engine) {
 	api.PUT("/mission-orbit-items", h.API_UpdateMissionOrbitItem)
 	api.DELETE("/mission-orbit-items", h.API_DeleteMissionOrbitItem)
 
-	api.GET("/missions/", h.API_GetMissionDraft)
-	api.GET("/mission", h.API_GetMissionsList)
+	api.GET("/mission", h.API_GetMissionDraft)
+	api.GET("/missions", h.API_GetMissionsList)
 	api.GET("/missions/:id", h.API_GetMissionDetail)
 	api.PUT("/missions/:id", h.API_UpdateMission)
 	api.PUT("/missions/:id/form", h.API_FormMission)
